9974df25170dde245370750ac221aed1769c8259
chusket <jesusgarciatudela@gmail.com>
2017-02-13 19:39:48 +0100

    deleting custom serelization and unserelization

--- Model/User.php
+++ Model/User.php
@@ -142,7 +142,7 @@ abstract class User extends AbstractedUser implements UserInterface
      * @var \DateTime
      */
     protected $credentialsExpireAt;
-    
+
     /**
      * @var boolean
      */
@@ -162,63 +162,6 @@ abstract class User extends AbstractedUser implements UserInterface
         parent::__construct();
     }
 
-    /**
-     * Serializes the user.
-     *
-     * The serialized data have to contain the fields used by the equals method and the username.
-     *
-     * @return string
-     */
-    public function serialize()
-    {
-        return serialize(array(
-            $this->password,
-            $this->salt,
-            $this->usernameCanonical,
-            $this->username,
-            $this->enabled,
-            $this->id,
-            $this->email,
-            $this->emailCanonical,
-            $this->locked,
-            $this->expired,
-            $this->credentialsExpired,
-        ));
-    }
-
-    /**
-     * Unserializes the user.
-     *
-     * @param string $serialized
-     */
-    public function unserialize($serialized)
-    {
-        $data = unserialize($serialized);
-
-        if (13 === count($data)) {
-            // Unserializing a User object from 1.3.x
-            unset($data[4], $data[5], $data[6], $data[9], $data[10]);
-            $data = array_values($data);
-        } elseif (11 === count($data)) {
-            // Unserializing a User from a dev version somewhere between 2.0-alpha3 and 2.0-beta1
-            unset($data[4], $data[7], $data[8]);
-            $data = array_values($data);
-        }
-
-        list(
-            $this->password,
-            $this->salt,
-            $this->usernameCanonical,
-            $this->username,
-            $this->enabled,
-            $this->id,
-            $this->email,
-            $this->emailCanonical,
-            $this->locked,
-            $this->expired,
-            $this->credentialsExpired,
-        ) = $data;
-    }
 
     /**
      * Returns a string representation.
